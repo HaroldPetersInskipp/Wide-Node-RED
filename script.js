6b2798e10e63624aff24145a154360a6522d78c8
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2025-09-01 08:25:24 -0600

    Update script.js
    Add update URL

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Wide Node-RED forum
 // @namespace    https://github.com/HaroldPetersInskipp/
-// @version      0.1.1
+// @version      0.1.2
 // @homepageURL  https://github.com/HaroldPetersInskipp/Wide-Node-RED
 // @supportURL   https://github.com/HaroldPetersInskipp/Wide-Node-RED/issues
+// @downloadURL  https://gist.github.com/HaroldPetersInskipp/8374a6ea6f3bb3f72517e9b4f0f941c6/raw/wide-node-red.user.js
+// @updateURL    https://gist.github.com/HaroldPetersInskipp/8374a6ea6f3bb3f72517e9b4f0f941c6/raw/wide-node-red.user.js
 // @description  Change Node-RED forum pages to be wide
 // @author       Inskipp
 // @copyright    2024+, HaroldPetersInskipp (https://github.com/HaroldPetersInskipp)
